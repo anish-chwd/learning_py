abebb2d27a9672a66dde712bd2daadb500dd584d
anish-chwd <245670617+anish-chwd@users.noreply.github.com>
2026-03-22 18:44:51 +0530

    initials

--- out.py
+++ out.py
@@ -1,6 +1,12 @@
 x = 5
-y = float(4)
+y = float(20)
 print(x, end=" ")
 print (y)
 print(type(x))
 print(type(y))
+
+if x > y:
+    print ("x is greter than y")
+    
+elif y > x:
+    print ("y is greater than x")
