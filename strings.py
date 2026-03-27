d8e148670b380d05f4f55e9d806d92c28da4ee5c
anish-chwd <245670617+anish-chwd@users.noreply.github.com>
2026-03-28 01:32:13 +0530

    stings:indexing

--- strings.py
+++ strings.py
@@ -3,3 +3,22 @@ print(a[2])
 
 for x in "banana":
     print (x)
+    
+print(len(a))
+
+print ("world" not in a)
+
+global txt
+txt = "the quick brown fox jumps over the lazy dog"
+
+if "quick" not in txt:
+    print("yes, 'quick' is present")
+    
+#slicing
+
+print (txt[5:10])
+print (txt[:20])
+print(txt[10:])
+print (txt[-15:-5])
+
+
