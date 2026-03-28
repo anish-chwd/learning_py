34425e9e8f865e28836ca5e1fa955ff7c3b83ad7
anish-chwd <245670617+anish-chwd@users.noreply.github.com>
2026-03-29 00:15:50 +0530

    aritmetic

---
+++ arithmetic.py
@@ -0,0 +1,19 @@
+# x = 236
+# y = 87
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y) #division returns a float
+# print(x % y)
+# print(x ** y)
+# print(x // y) #floor division an int(rounds DOWN)
+
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+if (count := len(nums)) > 3:
+    print(f"List has {count} numbers")
+    
+x = 100 
+print (1<x<1000)
+print(1<x and x<1000)
