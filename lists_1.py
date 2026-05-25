1c93b278806b87c895b96b50a7c4b5e746b7b6b0
ANISH CHOWDHURY <245670617+anish-chwd@users.noreply.github.com>
2026-05-25 18:59:36 +0000

    lists added

---
+++ lists_1.py
@@ -0,0 +1,7 @@
+nums = [0,1,2,3,4,5,6,7,8,9]
+num_1 = []
+
+for i in range(10):
+    num_1.append(nums[(9-i)])
+
+print(num_1)
---
+++ numbers_1.py
@@ -0,0 +1,10 @@
+amount = 45_00_000
+print (amount)
+
+x = y = z = 100
+print (y)
+
+PI = 3.414
+
+PI = 2 #this shit doesnt work
+print (PI)
