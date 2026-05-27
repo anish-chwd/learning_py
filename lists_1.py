908f66baeed1a6f846f22125d83ca687396644b1
ANISH CHOWDHURY <245670617+anish-chwd@users.noreply.github.com>
2026-05-27 08:31:59 +0000

    working on lists

--- lists_1.py
+++ lists_1.py
@@ -4,4 +4,36 @@ num_1 = []
 for i in range(10):
     num_1.append(nums[(9-i)])
 
-print(num_1)
+
+places = [
+    "Eiffel Tower",
+    "Times Square",
+    "Louvre Museum",
+    "Great Wall of China",
+    "Taj Mahal",
+    "Colosseum",
+    "Statue of Liberty",
+    "Sydney Opera House",
+    "Machu Picchu",
+    "Forbidden City",
+    "Angkor Wat",
+    "Notre-Dame Cathedral",
+    "Central Park",
+    "Las Vegas Strip",
+    "Empire State Building",
+    "Golden Gate Bridge",
+    "Lincoln Memorial",
+    "Sagrada Familia",
+    "Disneyland California",
+    "Magic Kingdom Orlando",
+    "Grand Canyon",
+    "Great Pyramid of Giza",
+    "Leaning Tower of Pisa",
+    "Palace of Versailles",
+    "Acropolis",
+    "Burj Khalifa",
+    "Big Ben",
+    "Tower Bridge",
+    "Buckingham Palace",
+    "Christ the Redeemer"
+]
---
+++ lists_2.py
@@ -0,0 +1,24 @@
+import lists_1 as lst
+
+# print (lst.places)
+
+lst.places.insert(0, "Victoria memorial")
+
+copy_places = []
+for i in range(len(lst.places)):
+    copy_places.append(lst.places[i])
+
+del lst.places[0]
+
+global mismatch
+
+for place in copy_places:
+    if place not in lst.places:
+        print(f"{place} got deleted")    
+
+var = copy_places.pop(0)
+print (f"var = {var}")
+
+for place in copy_places:
+    if place not in lst.places:
+        print(f"{place} got deleted")  
