cc85cf7b0317c164408d1ca97f51c50c9ee2bf30
ANISH CHOWDHURY <245670617+anish-chwd@users.noreply.github.com>
2026-06-13 04:48:13 +0000

    lists completion

--- lists_5.py
+++ lists_5.py
@@ -1,3 +1,15 @@
-num = list(range(1,101))
-squares = [num**2 for num in range(1,101)]
-print(squares)
+# num = list(range(1,101))
+# squares = [num**2 for num in range(1,101)]
+# print(squares)
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:2])
+print(players[-2:])
+
+for player in players[:2]:
+    print(player.upper())
+    
+stars = players
+
+players.append("chandra")
+print (stars)
