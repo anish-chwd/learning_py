1fb1cc369fbf1ee2af680b58a56c7960e045fb63
ANISH CHOWDHURY <245670617+anish-chwd@users.noreply.github.com>
2026-06-30 20:25:13 +0000

    conditional statements added

---
+++ conditional.py
@@ -0,0 +1,7 @@
+age = int(input("enter age --> "))
+if (age < 18 or age > 60):
+    print("Not Qualified due to age")
+    
+else :
+    print ("age accepted move ahead")
+
---
+++ conditional_2.py
@@ -0,0 +1,6 @@
+import lists_1 as lst 
+
+print ('Taj Mahal' in lst.places)
+
+if 'Central Park' in lst.places:
+    print ("Central park is in lst.places")
